63af7aae8ff39e175a95ee20fef067d7a0d94123
Chris Funderburg <c.funderburg@mak-system.net>
2024-08-22 02:31:48 +0100

    figuring out the right arn

--- lifecycle.tf
+++ lifecycle.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "dlm_lifecycle" {
       "ec2:DescribeSnapshots",
     ]
 
-    resources = ["arn:aws:sts:${data.aws_caller_identity.current.account_id}:*"]
+    resources = ["arn:aws:sts::${data.aws_caller_identity.current.account_id}:*"]
   }
 
   statement {
